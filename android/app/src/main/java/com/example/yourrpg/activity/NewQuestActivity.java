7bfbb915c7723a5f981c4f4f6cdcd983be968a5f
sc4mp <58878850+scaamp@users.noreply.github.com>
2023-04-07 14:28:20 +0200

    View from NewQuestActivity updated

--- android/app/src/main/java/com/example/yourrpg/activity/NewQuestActivity.java
+++ android/app/src/main/java/com/example/yourrpg/activity/NewQuestActivity.java
@@ -1,11 +1,15 @@
 package com.example.yourrpg.activity;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,7 +24,11 @@ import java.util.GregorianCalendar;
 public class NewQuestActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
     public static final String NEW_QUEST = "NEW_QUEST";
     private EditText questDeadlineEditText;
+    private EditText questDeadlineHourEditText;
+    private Spinner questStatSpinner;
+    private Spinner questStatPointsSpinner;
     private DateFormat dateFormat;
+    TimePickerDialog picker;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,7 +36,37 @@ public class NewQuestActivity extends AppCompatActivity implements DatePickerDia
         setContentView(R.layout.activity_new_questlog);
         setTitle("New quest");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        questStatSpinner = (Spinner) findViewById(R.id.questStatSpinner);
+        ArrayAdapter<CharSequence> questStatAdapter = ArrayAdapter.createFromResource(getApplicationContext(),
+                R.array.stats_array, android.R.layout.simple_spinner_item);
+        questStatAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        questStatSpinner.setAdapter(questStatAdapter);
+
+        questStatPointsSpinner = (Spinner) findViewById(R.id.questStatPointsSpinner);
+        ArrayAdapter<CharSequence> questStatPointsAdapter = ArrayAdapter.createFromResource(getApplicationContext(),
+                R.array.statsPoints_array, android.R.layout.simple_spinner_item);
+        questStatPointsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        questStatPointsSpinner.setAdapter(questStatPointsAdapter);
+
         questDeadlineEditText = (EditText) findViewById(R.id.questDeadlineEditText);
+        questDeadlineHourEditText = (EditText) findViewById(R.id.questDeadlineHourEditText);
+        questDeadlineHourEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar cldr = Calendar.getInstance();
+                int hour = cldr.get(Calendar.HOUR_OF_DAY);
+                int minutes = cldr.get(Calendar.MINUTE);
+                picker = new TimePickerDialog(NewQuestActivity.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker tp, int sHour, int sMinute) {
+                                questDeadlineHourEditText.setText(sHour + ":" + sMinute);
+                            }
+                        }, hour, minutes, true);
+                picker.show();
+            }
+        });
         questDeadlineEditText.setText(getCurrentDate());
         questDeadlineEditText.setOnClickListener(new View.OnClickListener() {
             @Override
